91978b361798b2eccd553ab297867379059f3a3c
aguila150 <munoz09sebastian@gmail.com>
2026-05-13 12:03:01 -0300

    segundo commit

--- src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
+++ src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
@@ -1,19 +1,32 @@
 package edu.etec.ds.fundamentos
 
 fun evaluarPositivo(numero: Int): String {
-    TODO("Retornar \"Positivo\" si numero > 0, sino \"No positivo\"")
+    return if (numero > 0) {
+        "Positivo"
+    } else {
+        "No positivo"
+    }
 }
 
 fun clasificarNumero(numero: Int): String {
-    TODO("Clasificar: Mayor a 10, Menor a 10, o Igual a 10")
+    return if (numero > 0) {
+        " no positivo"
+    } else {
+        "positivo"
+    }
 }
 
+
 fun evaluarEdad(edad: Int): String {
-    TODO("Menor de edad: < 18, Adulto: 18-65, Adulto mayor: > 65")
+    return if (edad > 0) {
+        "Positivo"
+    } else {
+        "No positivo"
+    }
 }
 
 fun obtenerDescuento(monto: Double): Double {
-    TODO("Retornar descuento: 10% si monto > 100, 20% si > 150, 30% si > 200, sino 0%")
+        
 }
 
 fun calcular(operador: Char, a: Int, b: Int): Double {
